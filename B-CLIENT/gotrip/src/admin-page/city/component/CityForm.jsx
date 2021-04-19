fd5f426e992398a6bd9a3a020a7afb7a51e247a8
DuongPhamViet <duongpd12@gmail.com>
2021-04-19 19:29:52 +0700

    Add new City

---
+++ B-CLIENT/gotrip/src/admin-page/city/component/CityForm.jsx
@@ -0,0 +1,55 @@
+const CityForm = ({
+  city,
+  onSaveFormChange,
+  onClose,
+  onSaveCity
+}) => {
+  const { id, name, country, status } = city;
+
+  const onHandlePropertyChange = (e) => {
+    city[e.target.name] = e.target.value;
+    onSaveFormChange(city);
+  }
+
+  return (
+    <form id="addNew">
+      <div className="card-body">
+        <div className="form-group">
+          <label for="exampleInputEmail1">Id</label>
+          <input type="text" name="id" className="form-control" id="id" placeholder="Id" value={id} onChange={onHandlePropertyChange} />
+        </div>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Name</label>
+          <input type="text" name="name" className="form-control" id="name" placeholder="Name" value={name} onChange={onHandlePropertyChange} />
+        </div>
+        <div className="form-group">
+          <label for="exampleInputPassword1">Country</label>
+          <select className="form-select form-control" aria-label="Default select example" name="country" value={country} onChange={onHandlePropertyChange}>
+            <option selected hidden>Chọn tên thành phố</option>
+            <option value=""></option>
+            <option value="Việt Nam">Việt Nam</option>
+            <option value="United State" >United State</option>
+            <option value="Trung Quốc" >Trung Quốc</option>
+            <option value="Ấn Độ" >Ấn Độ</option>
+          </select>
+        </div> 
+        <div className="form-group">
+          <label for="exampleInputPassword1">Status</label>
+          <select class="form-control" name="status" value={status} onChange={onHandlePropertyChange} >
+            <option value=""></option>
+            <option value="Actived">Actived</option>
+            <option value="Disabled">Disabled</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-12 d-flex justify-content-end">
+            <button type="button" className="btn btn-danger mr-5" onClick={onClose}>Close</button>
+            <button type="button" className="btn btn-info" onClick={() => onSaveCity(city)}>Submit</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default CityForm;
---
+++ B-CLIENT/gotrip/src/admin-page/city/component/CitySearch.jsx
@@ -0,0 +1,85 @@
+const CitySearch = ({
+    searchParam,
+    onHandleSearchChange,
+    onHandleSearch,
+    onHandleResetForm
+}) => {
+
+    const { cityName, cityCountry, status } = searchParam;
+
+    const onHandleFieldChange = (e) => {
+        onHandleSearchChange({ ...searchParam, [e.target.name]: e.target.value });
+    };
+
+    const onSearch = (searchParam) => {
+        onHandleSearch(searchParam);
+    };
+
+    return (
+        <div className="card card-info">
+            <div className="card-header">
+                <h3 className="card-title">Search Criteria</h3>
+            </div>
+            <form className="form-horizontal" autoComplete="off">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <div className="form-group row">
+                                <label for="inputEmail3" className="col-sm-2 col-form-label">Name</label>
+                                <div className="col-sm-10">
+                                    <input type="text"
+                                        className="form-control"
+                                        id="cityName"
+                                        name="cityName"
+                                        placeholder="City Name"
+                                        value={cityName}
+                                        onChange={onHandleFieldChange}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="form-group row">
+                                <label for="inputPassword3" className="col-sm-2 col-form-label">Country</label>
+                                <div className="col-sm-10">
+                                    <select class="form-control" value={cityCountry} id="cityCountry"
+                                        name="cityCountry" onChange={onHandleFieldChange}>
+                                        <option selected hidden>Chọn tên thành phố</option>
+                                        <option value=""></option>
+                                        <option value="Việt Nam">Việt Nam</option>
+                                        <option value="United State" >United State</option>
+                                        <option value="Trung Quốc" >Trung Quốc</option>
+                                        <option value="Ấn Độ" >Ấn Độ</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <div className="form-group row">
+                                <label for="inputPassword3" className="col-sm-2 col-form-label">Status</label>
+                                <div className="col-sm-10">
+                                    <select class="form-control" value={status} name="status" onChange={onHandleFieldChange}>
+                                        <option selected hidden>Chọn trạng thái</option>
+                                        <option value=""></option>
+                                        <option value="Actived">Actived</option>
+                                        <option value="Disabled">Disabled</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-sm-12 d-flex justify-content-end">
+                            <button type="button" className="btn btn-info mr-5" onClick={() => onSearch(searchParam)}>Search</button>
+                            <button type="button" className="btn btn-info" onClick={onHandleResetForm}>Reset</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+export default CitySearch;
