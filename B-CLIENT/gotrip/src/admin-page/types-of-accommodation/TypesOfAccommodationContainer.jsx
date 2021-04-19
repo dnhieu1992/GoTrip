86b26c0e5b79d03fe8f928b6e8e0c7e87bfbe3bc
DESKTOP-OFQ3B2L\CHUNG <chungdaotn@gmail.com>
2021-04-19 20:12:12 +0700

    dao nguyen chung

--- B-CLIENT/gotrip/src/admin-page/types-of-accommodation/TypesOfAccommodationContainer.jsx
+++ B-CLIENT/gotrip/src/admin-page/types-of-accommodation/TypesOfAccommodationContainer.jsx
@@ -1,8 +1,117 @@
-import TypesOfAccommodation from './component/TypesOfAccommodation';
+
+import { useState } from 'react';
+import Modal from '../../shared/components/forms/Modal';
+import TypesOfAccommodationGrid from './component/TypesOfAccommodationGrid';
+import TypesOfAccommodationSearch from './component/TypesOfAccommodationSearch';
+import TypesOfAccommodationForm from './component/TypesOfAccommodationForm';
 
 const TypesOfAccommodationContainer = () => {
+
+
+    const mockDatas = [
+        { id: 1, name: 'Hotels', amount: 769854, status: 'Actived' },
+        { id: 2, name: 'Apartments', amount: 699393, status: 'Actived' },
+        { id: 3, name: 'Resorts', amount: 18212, status: 'Actived' },
+        { id: 4, name: 'Villas', amount: 368425, status: 'Actived' },
+        { id: 5, name: 'Cabins', amount: 29531, status: 'Actived' },
+        { id: 6, name: 'Cottages', amount: 129312, status: 'Actived' },
+        { id: 7, name: 'Glamping', amount: 9482, status: 'Actived' },
+        { id: 8, name: 'Serviced Apartments', amount: 31594, status: 'Actived' }
+    ];
+
+    const [ data, setData] = useState(mockDatas.slice(0, 5));
+    const [ searchParam, setSearchParam] = useState({});
+    const [ options, setOptions] = useState({ currentPage: 1});
+    const [ isShow, setIsShow] = useState(false);
+    const [ typesOfAccommodation, setTypesOfAccommodation] = useState({});
+
+    const onHandleSearchChange = (param) => {
+        setSearchParam(param);
+    }
+
+    const onHandleSearch = ({typesOfAccommodationName, status}) => {
+        const accommodations = mockDatas.filter((item) => {
+            if((!typesOfAccommodationName || (item.name.toLowerCase().indexOf(typesOfAccommodationName.toLowerCase()) > -1))
+            && (!status || (item.status.indexOf(status) >-1))) {
+                return item;
+            }
+        });
+        const result = accommodations.slice((options.currentPage - 1) * options.pageSize, options.currentPage * options.pageSize)
+        setData(accommodations);
+    }
+
+    const onHandlePageChange = (pageNumber) => {
+        const result = mockDatas.slice((pageNumber - 1) * options.pageSize, pageNumber * options.pageSize);
+        setData(result);
+        setOptions({ ...options, currentPage: pageNumber});
+
+    }
+
+    const onHandleResetForm = () => {
+        setSearchParam({});
+        const result = mockDatas.slice((options.currentPage - 1) * options.pageSize, options.currentPage * options.pageSize);
+        setData(result);
+    }
+
+    const addNewForm = () => {
+        setIsShow(true);
+    }
+
+    const onClose = () => {
+        setIsShow(false);
+        setTypesOfAccommodation({});
+    }
+
+    const onSaveTypesOfAccommodation = () => {
+        data.push(typesOfAccommodation);
+        setData(data);
+        onClose();
+    }
+
+    const onSaveFormChange = (typesOfAccommodation) => {
+        console.log("typesOfAccommodation: ",typesOfAccommodation);
+        setTypesOfAccommodation(typesOfAccommodation);
+    }
+
+    const modalRender = () => {
+        return (
+            <Modal classNames = {'modal-lg'}
+                title = "Add New Types Of Accommodation"
+                onClose = {onClose}
+                onSave = {onSaveTypesOfAccommodation}>
+                <TypesOfAccommodationForm 
+                    typesOfAccommodation = {typesOfAccommodation}
+                    onSaveFormChange = {onSaveFormChange}
+                    onClose = {onClose}
+                    onSaveTypesOfAccommodation = {onSaveTypesOfAccommodation}/>
+                
+            </Modal>
+        )
+    }
     return (
-        <TypesOfAccommodation/>
+        <>
+            {isShow && modalRender()}
+            <div className = "card">
+                <div className="card-header text-uppercase">
+                    <h3>Types-Of-Accommodation</h3>
+                </div>
+                <div className="card-body">
+                <TypesOfAccommodationSearch
+                    searchParam = {searchParam}
+                    onHandleSearchChange = {onHandleSearchChange}
+                    onHandleSearch = {onHandleSearch}
+                    onHandleResetForm = {onHandleResetForm}
+                />
+                <TypesOfAccommodationGrid
+                    data = {data}
+                    options = {options}
+                    totalItems = {mockDatas.length}
+                    onHandlePageChange = {onHandlePageChange}
+                    addNewForm = {addNewForm}
+                />
+                </div>
+            </div>
+        </>
     )
 }
 
---
+++ B-CLIENT/gotrip/src/admin-page/types-of-accommodation/component/TypesOfAccommodationSearch.jsx
@@ -0,0 +1,66 @@
+const typesOfAccommodationSearch = ({
+    searchParam,
+    onHandleSearchChange,
+    onHandleSearch,
+    onHandleResetForm
+}) => {
+
+    const { typesOfAccommodationName, status} = searchParam;
+
+    const onHandleFieldChange = (e) => {
+        onHandleSearchChange({ ...searchParam, [e.target.name]: e.target.value});
+
+    };
+
+    const onSearch = (searchParam) => {
+        onHandleSearch(searchParam);
+    }
+
+    return(
+        <div className="card card-info">
+            <div className="card-header">
+                <h3 className="card-title">Search</h3>
+            </div>
+            <form className="form-horizontal" autoComplete = "off">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <div className="form-group row">
+                                <label for = "inputEmail3" className="col-sm-2 col-form-label">Name</label>
+                                <div className="col-sm-10">
+                                    <input type="text"
+                                        className = "form-control"
+                                        id="name"
+                                        name = "typesOfAccommodationName"
+                                        placeholder = "Name"
+                                        value = {typesOfAccommodationName}
+                                        onChange = {onHandleFieldChange}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="form-group row">
+                                <label for = "inputPasswword3" className = "col-sm-2 col-form-label">Status</label>
+                                <div className="col-sm-10">
+                                    <select className = "form-control" value = {status} name = "status" onChange = {onHandleFieldChange}>
+                                        <option value=""></option>
+                                        <option value="Actived">Actived</option>
+                                        <option value="Disabled">Disabled</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-12 d-flex justify-content-end">
+                            <button type = "button" className = "btn btn-info mr-5" onClick ={() => onSearch(searchParam)} >Search</button>
+                            <button type = "button" className = "btn btn-info" onClick = {onHandleResetForm}>Reset</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+export default typesOfAccommodationSearch;
