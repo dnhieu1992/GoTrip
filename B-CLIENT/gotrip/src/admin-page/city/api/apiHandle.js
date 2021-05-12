5cf05b3109ab95470b456bb19efce9fa94bd16f8
DuongPhamViet <duongpd12@gmail.com>
2021-05-12 14:35:08 +0700

    Centralized state of city management

--- B-CLIENT/gotrip/src/admin-page/city/api/apiHandle.js
+++ B-CLIENT/gotrip/src/admin-page/city/api/apiHandle.js
@@ -1,50 +1,85 @@
-const getCities = async (params) => {
+import { cleanObject } from '../../shared/ultils/ultils';
+import alertNotify from '../../../shared/ultils/alertNotify.js';
+
+const getCities = async (params, onSuccess, onError) => {
     try {
         const url = new URL("http://localhost:5000/api/city/search");
+
+        params = cleanObject(params);
         Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+
         const res = await fetch(url);
 
         if (!res.ok) {
             throw new Error(res.status);
         }
+
         const data = await res.json();
-        return data;
+        if (onSuccess) {
+            return onSuccess(data);
+        }
+
     } catch (error) {
-        console.log(error);
+        alertNotify.error(error);
+        if (onError) {
+            return onError();
+        }
     }
 }
-const createNewCity = async (city) => {
+const createNewCity = async (city, onSuccess, onError) => {
     try {
         await fetch("http://localhost:5000/api/city/create", {
             method: "POST",
             body: JSON.stringify(city),
             headers: { "Content-type": "application/json; charset=UTF-8" }
         });
-    } catch {
-        console.log(error);
+
+        if (!res.ok && res.status === 409) {
+            throw "The item exists.";
+        }
+
+        alertNotify.success("Create new a country success.");
+
+        if (onSuccess) {
+            return onSuccess();
+        }
+    } catch (error) {
+        alertNotify.error(error);
     }
 }
 
-const updateCity = async (city) => {
+const updateCity = async (city, onSuccess, onError) => {
     try {
         await fetch("http://localhost:5000/api/city/update", {
             method: "PUT",
             body: JSON.stringify(city),
             headers: { "Content-type": "application/json; charset=UTF-8" }
         });
-    } catch {
-        console.log(error);
+
+        alertNotify.success("Update the city success.");
+
+        if (onSuccess) {
+            return onSuccess();
+        }
+    } catch (error) {
+        alertNotify.error(error);
     }
 }
 
-const deleteCity = async (id) => {
+const deleteCity = async (id, onSuccess, onError) => {
     try {
         await fetch(`http://localhost:5000/api/city/delete/${id}`, {
             method: "DELETE",
             headers: { "Content-type": "application/json; charset=UTF-8" }
         });
-    } catch {
-        console.log(error);
+
+        alertNotify.error("Delete the city success.");
+
+        if (onSuccess) {
+            return onSuccess();
+        }
+    } catch (error) {
+        alertNotify.error(error);
     }
 }
 
--- B-CLIENT/gotrip/src/admin-page/city/component/CityForm.jsx
+++ B-CLIENT/gotrip/src/admin-page/city/component/CityForm.jsx
@@ -1,5 +1,9 @@
+import classNames from 'classnames';
+
 const CityForm = ({
   city,
+  isValid,
+  errorMessage,
   countries,
   onSaveFormChange,
   onClose,
@@ -11,21 +15,51 @@ const CityForm = ({
     status
   } = city;
 
+  const {
+    cityNameErrorMsg,
+    countryNameErrorMsg,
+    cityStatusErrorMsg
+  } = errorMessage
+
   const onHandlePropertyChange = (e) => {
-    onSaveFormChange({ ...city, [e.target.name]: e.target.value });
-  }
+    if (e?.target) {
+      onSaveFormChange({
+        ...city,
+        [e.target.name]: e.target.value
+      });
+    }
+  };
 
   return (
     <form id="addNew">
       <div className="card-body">
         <div className="form-group">
           <label for="exampleInputEmail1">Name</label>
-          <input type="text" name="name" className="form-control" id="name" placeholder="Name" value={name} onChange={onHandlePropertyChange} />
+          <input
+            type="text"
+            name="name"
+            className={classNames("form-control", { "is-invalid": cityNameErrorMsg })}
+            id="name"
+            placeholder="Name"
+            value={name}
+            onChange={onHandlePropertyChange}
+          />
+          {cityNameErrorMsg && (
+            <div className="invalid-feedback">
+              {cityNameErrorMsg}
+            </div>
+          )}
         </div>
         <div className="form-group">
           <label for="exampleInputPassword1">Country</label>
-          <select className="form-select form-control" aria-label="Default select example" name="countryId" value={countryId} onChange={onHandlePropertyChange}>
-            <option selected hidden>Choose the country...</option>
+          <select
+            className={classNames("form-control", { "is-invalid": countryNameErrorMsg })}
+            aria-label="Default select example"
+            name="countryId"
+            value={countryId}
+            onChange={onHandlePropertyChange}
+          >
+            <option hidden>Choose the country...</option>
             <option value=""></option>
             {countries.map(country => {
               return (
@@ -33,20 +67,48 @@ const CityForm = ({
               )
             })}
           </select>
+          {countryNameErrorMsg && (
+            <div className="invalid-feedback">
+              {countryNameErrorMsg}
+            </div>
+          )}
         </div>
         <div className="form-group">
           <label for="exampleInputPassword1">Status</label>
-          <select class="form-control" name="status" value={status} onChange={onHandlePropertyChange} >
-            <option selected hidden>Choose a status...</option>
+          <select
+            className={classNames("form-control", { "is-invalid": cityStatusErrorMsg })}
+            name="status"
+            value={status}
+            onChange={onHandlePropertyChange}
+          >
+            <option hidden>Choose a status...</option>
             <option value=""></option>
             <option value="Actived">Actived</option>
             <option value="Disabled">Disabled</option>
           </select>
+          {cityStatusErrorMsg && (
+            <div className="invalid-feedback">
+              {cityStatusErrorMsg}
+            </div>
+          )}
         </div>
         <div className="form-group">
           <div className="col-sm-12 d-flex justify-content-end">
-            <button type="button" className="btn btn-danger mr-5" onClick={onClose}>Close</button>
-            <button type="button" className="btn btn-info" onClick={() => onSaveCity(city)}>Submit</button>
+            <button
+              type="button"
+              className="btn btn-danger mr-5"
+              onClick={() => onClose(false)}
+            >
+              Close
+            </button>
+            <button
+              type="button"
+              className="btn btn-info"
+              disabled={!isValid}
+              onClick={() => onSaveCity(city)}
+            >
+              Submit
+            </button>
           </div>
         </div>
       </div>
