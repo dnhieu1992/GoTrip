d1c08724a5da4d3cfe9025ece07954fafc9828f7
dnhieu1992 <dnhieu1992@gmail.com>
2021-06-05 00:47:37 +0700

    update handling error in login service

--- B-CLIENT/gotrip/src/auth/login/api/apiHandle.js
+++ B-CLIENT/gotrip/src/auth/login/api/apiHandle.js
@@ -2,21 +2,24 @@ import { API } from '../constant/api';
 
 const loginUser = async (account, onSuccess, onError) => {
     try {
-        debugger
-        const data = await fetch(API.LOGIN_USER, {
+        const res = await fetch(API.LOGIN_USER, {
             method: "POST",
             body: JSON.stringify(account),
             headers: { "Content-type": "application/json; charset=UTF-8" }
         });
 
+        const data = await res.json();
+
+        if (!res.ok && res.status === 401) {
+            throw data.error
+        }
+
         if (onSuccess) {
             return onSuccess();
         }
     } catch (error) {
-        console.log(error);
-        debugger
         if (onError) {
-            return onError();
+            return onError(error);
         }
     }
 }
