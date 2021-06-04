9e23315d4ecaf57653d66d591def308c91a502f9
dnhieu1992 <dnhieu1992@gmail.com>
2021-06-04 11:45:29 +0700

    Update git ignore

--- A-SERVER/src/config/db.config.js
+++ A-SERVER/src/config/db.config.js
@@ -2,6 +2,6 @@ const DB_CONFIG = {
     HOST: 'mongodb://127.0.0.1',
     PORT: 27017,
     DB: 'gotrip',
-    MONGO_URI: 'mongodb+srv://sa:qaz@123@gotrip.tjh8n.mongodb.net/gotrip?retryWrites=true&w=majority'
+    MONGO_URI: ''
 }
 export default DB_CONFIG;
