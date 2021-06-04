12b2bdfa3f9bdae436ee34d74f9ef8b0e180689b
dnhieu1992 <dnhieu1992@gmail.com>
2021-06-04 10:51:30 +0700

    remove db url

--- A-SERVER/src/config/db.config.js
+++ A-SERVER/src/config/db.config.js
@@ -2,6 +2,5 @@ const DB_CONFIG = {
     HOST: 'mongodb://127.0.0.1',
     PORT: 27017,
     DB: 'gotrip',
-    MONGO_URI: 'mongodb+srv://sa:qaz@123@gotrip.tjh8n.mongodb.net/gotrip?retryWrites=true&w=majority'
 }
 export default DB_CONFIG;
