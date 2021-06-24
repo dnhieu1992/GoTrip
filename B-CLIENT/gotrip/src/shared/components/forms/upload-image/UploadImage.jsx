43e684840b2d0457d212d80318553a291dbe8d77
chungdao <chungdaotn@gmail.com>
2021-06-24 21:12:08 +0700

    upload image in form create

--- B-CLIENT/gotrip/src/shared/components/forms/upload-image/UploadImage.jsx
+++ B-CLIENT/gotrip/src/shared/components/forms/upload-image/UploadImage.jsx
@@ -1,24 +1,61 @@
+import { useState } from "react";
+
 const UploadImage = () => {
+    const [state,setState]=useState({});
+    const [showRemove,setShowRemove]=useState(false);
+    const {
+        imagePreviewUrl,
+        file,
+    }=state;
+
+    const onHandleImageChange = (image)=>{
+        debugger;
+        const reader=new FileReader();
+        const file=image.target.files[0];
+
+        reader.onloadend=()=>{
+            setState({
+                ...state,
+                imagePreviewUrl:reader.result,
+                file
+            })
+        }            
+
+        if(!imagePreviewUrl){
+            setShowRemove(true)
+        }
+
+        reader.readAsDataURL(file);
+    }
+
+
+    const onCancelImage = ()=>{
+        debugger;
+        setState({
+            ...state,
+            imagePreviewUrl:null,
+            file:null
+        })
+        setShowRemove(false)
+    }
+
+    const Results=()=>(
+        <span className="remove-image"  onClick={onCancelImage}>x</span>
+    )
+
     return (
         <div className="image-preview">
-            <h1 className="image-preview__header">Upload image</h1>
             <div className="image-preview__form ">
-                <div className="image-preview__form-button">
-                    <span className="btn btn-default btn-file">
-                        <i className="fa fa-cloud-upload"></i> &nbsp;
-                        Choose File 
-                        <input type="file" id="imgInp" /> 
-                    </span>
-                </div>
-                <div className="image-preview__form-title mt-3 d-flex justify-content-center">
-                    <span>
-                        The image uploaded will be rendered in side the box below.
-                    </span>
-                </div>
                 <div className="image-preview__form-image">
-                    <img id="img" />
+                    <img id="img" src={imagePreviewUrl} ></img>
+                    {/* <a className="remove-image"  onClick={onCancelImage}>x</a> */}
+                    { showRemove ? <Results /> : null }
                 </div>
-                
+                <div className="image-preview__form-button mb-3">
+                <span class="btn btn-default btn-file">
+                    Choose File <input type="file" id="imgInp" onChange={onHandleImageChange} />
+                </span>
+                </div>
             </div>
         </div>
     )
