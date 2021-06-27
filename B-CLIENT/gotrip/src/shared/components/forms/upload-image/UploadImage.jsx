9d0f31ecfbbd11193324c31b5fdc1a107198cb10
chungdao <chungdaotn@gmail.com>
2021-06-27 20:22:09 +0700

    fix css upload image form

--- B-CLIENT/gotrip/src/shared/components/forms/upload-image/UploadImage.jsx
+++ B-CLIENT/gotrip/src/shared/components/forms/upload-image/UploadImage.jsx
@@ -35,18 +35,21 @@ const UploadImage = () => {
     return (
         <div className="image-preview">
             <div className="image-preview__form ">
+                <div className="image-preview__form-button ">
+                    <span class="btn btn-default btn-file">
+                        <i className="fa fa-cloud-upload"/>
+                        Choose File Your Computer
+                        <input ref={fileRef} type="file" id="imgInp" onChange={onHandleImageChange} />
+                    </span>
+                </div>
                 <div className="image-preview__form-image">
                     {imagePreviewUrl && (
                         <>
                             <img id="img" src={imagePreviewUrl} />
-                            <span className="remove-image" onClick={removeImage}>x</span>
+                            {/* <span className="remove-image" onClick={removeImage}>x</span> */}
+                            <i className="fa fa-times" onClick={removeImage} ></i>
                         </>
                     )}
-                </div>
-                <div className="image-preview__form-button mb-3">
-                <span class="btn btn-default btn-file">
-                    Choose File <input ref={fileRef} type="file" id="imgInp" onChange={onHandleImageChange} />
-                </span>
                 </div>
             </div>
         </div>
