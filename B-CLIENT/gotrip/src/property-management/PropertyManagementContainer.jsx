ba8950275d2c4e8c69696edf31c9944ed6b7157f
chungdao <chungdaotn@gmail.com>
2021-07-10 10:40:09 +0700

    fix UI of property management

--- B-CLIENT/gotrip/src/property-management/PropertyManagementContainer.jsx
+++ B-CLIENT/gotrip/src/property-management/PropertyManagementContainer.jsx
@@ -16,14 +16,25 @@ const PropertyManagementContainer = ()=>{
 
     return(
         <div className="property-mng">
-            <div className="property-mng__header text uppercase">
-                <h3>Booking.com</h3>
-                <i className="fa fa-globe"></i>
+            <div className="property-mng__header text uppercase row">
+                <img className="img1" src="https://static.booking.com/static/img/b26logo/booking_logo_retina_white.png" alt="" />
+                <img className="img2" src="https://free.vector6.com/wp-content/uploads/2021/05/PNG-0000002045-png-hanh-tinh-vu-tru.png" alt="" />
             </div>
-            <div className="property-mng__body row">
+            <div className="property-mng__body">
+            <div className="property-mng__body__title">
+                <div className="property-mng__body__title -welcoming">
+                    List your property on Booking.com and start welcoming guests in no time!
+                </div>
+                <div className="property-mng__body__title -start">
+                    To get started, select the type of property you want to list on Booking.com
+                </div>
+            </div>
+            <div className="property-mng__body__list row">
+
                     <PropertyList
                     data={properties}
                 />
+            </div>
             </div>
         </div>
     )
--- B-CLIENT/gotrip/src/property-management/component/PropertyList.jsx
+++ B-CLIENT/gotrip/src/property-management/component/PropertyList.jsx
@@ -1,29 +1,29 @@
 import PropTypes from 'prop-types';
 
-const PropertyList=(
-    {data}
-)=>{
-    const properties=data.map(property=>{
-        return(
+const PropertyList = (
+    { data }
+) => {
+    const properties = data.map(property => {
+        return (
             <div className="property-list">
-                    <div className="property-list__image">
-                        <i className="fa fa-home"></i>
-                    </div>
-                    <div className="property-list__name">
-                        {property.name}
-                    </div>
-                    <div className="property-list__description">
-                        {property.description}
-                    </div>
-                    <div className="property-list__button ">
-                        <button className="btn btn-primary">List your property</button>
-                    </div>
-               
+                <div className="property-list__image">
+                    <img src="https://q.bstatic.com/static/img/join/segmentation/accomm_one_apt_main@2x.png" alt="" />
+                </div>
+                <div className="property-list__name">
+                    {property.name}
+                </div>
+                <div className="property-list__description">
+                    {property.description}
+                </div>
+                <div className="property-list__button ">
+                    <button className="btn btn-primary">List your property</button>
+                </div>
+
             </div>
-            
+
         )
     })
-    return(
+    return (
         // {properties}
         <>
             {properties}
@@ -32,10 +32,10 @@ const PropertyList=(
 }
 export default PropertyList;
 
-PropertyList.propTypes={
-    data:PropTypes.array
+PropertyList.propTypes = {
+    data: PropTypes.array
 }
 
-PropertyList.defaultProps={
-    data:[]
+PropertyList.defaultProps = {
+    data: []
 }
